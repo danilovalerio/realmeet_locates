9f1c950d47b78fcea4d03348d0c2c2c45b35b4de
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-09-15 13:08:21 -0300

    tratando exception generica e prettier ok

--- service/src/main/java/br/com/sw2you/realmeet/config/ControllerExceptionHandler.java
+++ service/src/main/java/br/com/sw2you/realmeet/config/ControllerExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 /**
  * Controller responsável por capturar os erros que ocorrem na aplicação
@@ -13,6 +14,25 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
  */
 @RestControllerAdvice
 public class ControllerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> onException(Exception exception) {
+        System.out.println("Exception: " + exception);
+        if (((MethodArgumentTypeMismatchException) exception).getCause().toString().contains("NumberFormatException")) {
+            return exceptionGeneric(exception);
+        }
+        return exceptionGeneric(exception);
+    }
+
+    public ResponseEntity<Object> exceptionGeneric(Exception exception) {
+        return new ResponseEntity<>(
+            new ResponseError()
+                .status(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                .code(HttpStatus.BAD_REQUEST.value())
+                .message(exception.getMessage()),
+            HttpStatus.BAD_REQUEST
+        );
+    }
 
     @ExceptionHandler(RoomNotFoundException.class)
     public ResponseEntity<Object> handleNotFoundException(Exception exception) {
