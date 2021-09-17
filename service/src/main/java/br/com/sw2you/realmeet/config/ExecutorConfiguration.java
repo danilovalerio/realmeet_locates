0457bb5fc92b0b17c144e2b55cd3a13f9a5e88b6
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-09-17 17:36:24 -0300

    adjust life time of thread

--- service/src/main/java/br/com/sw2you/realmeet/config/ExecutorConfiguration.java
+++ service/src/main/java/br/com/sw2you/realmeet/config/ExecutorConfiguration.java
@@ -16,7 +16,7 @@ public class ExecutorConfiguration {
         @Value("${realmeet.taskExecutor.pool.coreSize:20}") int corePoolSize,
         @Value("${realmeet.taskExecutor.pool.maxSize:20}") int maxPoolSize,
         @Value("${realmeet.taskExecutor.pool.queueCapacity:50}") int queueCapacity,
-        @Value("${realmeet.taskExecutor.pool.keepAliveSeconds:60}") int keepAliveSeconds
+        @Value("${realmeet.taskExecutor.pool.keepAliveSeconds:600000}") int keepAliveSeconds
     ) {
         /**
          * Retorna o pool de threads
